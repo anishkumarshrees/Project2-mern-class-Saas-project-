7d7d465a9906ad69dd09752e3894058f536783c5
anishkumarshrees <anishshrees12@gmail.com>
2025-06-01 20:43:28 +0545

    auth

---
+++ src/controller/globals/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+//functional style we use this style in before project
+exports.registerUser = ()=>{
+
+}
+
+exports.loginUser = ()=>{
+
+}
+//oop style but this is good for good programing
+class AuthController{
+    static async registerUser(){
+
+    }
+    static async loginUser(){
+
+    }
+}
+
+
+module.exports = AuthController
