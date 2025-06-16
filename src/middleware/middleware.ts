a0491737a9e14281dd2ea9d4337933c624fb44a8
anishkumarshrees <anishshrees12@gmail.com>
2025-06-16 20:13:20 +0545

    error handeling

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,5 +1,7 @@
 import {NextFunction, Request,Response} from "express"
 import Jwt  from "jsonwebtoken"
+import User from "../database/models/usermodel"
+import { IExtendedRequest } from "./type"
 
 
 //type maa add garna paryo vani talako steps garni
@@ -10,17 +12,18 @@ import Jwt  from "jsonwebtoken"
 // }
 
 
+
 class Middleware{
     // hello(req:Extend,res:Response)
     //route maa next steps garna ko lagi next function halnu parxa
-  static  isLoggedIn(req:Request,res:Response,next:NextFunction){
+  static  isLoggedIn(req:IExtendedRequest,res:Response,next:NextFunction){
     //steps 
     //check if login or not
     //token accept
     //verify token it is legit or not
     //headers maa herxam token
     // req.user.name esto bahiri kura extend garna lai mathi ko step follow garni
-  const token=  req.headers.authorization
+  const  token=  req.headers.authorization
   if(!token){
     res.status(401).json({
         message:"please provide token"
@@ -28,18 +31,29 @@ class Middleware{
     return
   }
   //verify garni steps
-  Jwt.verify(token,'thisissecret',(erroraayo,resultaayo)=>{
+  Jwt.verify(token,'thisissecret',async(erroraayo,resultaayo:any)=>{
     if(erroraayo){
         res.status(401).json({
             message:"invalid token"
         })
     }else{
         //token verify garni
-        console.log(resultaayo,'successfully logged in')
-        
+        // console.log(resultaayo,'successfully logged in')
+        const userData=await User.findByPk(resultaayo.id)
+        if(!userData){
+            res.status(403).json({
+                message:"user not found"
+            })
+
+        }
+        else{
+            req.user? : userData
+            next()
+        }
+
     }
   })
-next()
+
     }
 }
 
